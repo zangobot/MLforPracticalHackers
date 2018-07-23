9129a953c9a5ab76bad438df9a057e54e3ddfb2b
Luca Demetrio <luca.demetrio@outlook.it>
2018-07-23 14:38:48 +0200

    Fixed NN, added files for final challenge

--- nn.py
+++ nn.py
@@ -9,8 +9,8 @@ from sklearn.model_selection import train_test_split, cross_val_score
 
 X,Y = make_moons(n_samples=600, noise=0.15)
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
-plt.scatter(X[:,0],X[:,1],c=Y)
 plt.show()
+
 model = keras.Sequential(
     [
         keras.layers.Dense(2, activation=keras.activations.sigmoid, input_shape=X[0].shape, kernel_initializer=keras.initializers.RandomNormal()),
@@ -18,7 +18,7 @@ model = keras.Sequential(
         keras.layers.Dense(1, activation=keras.activations.sigmoid, kernel_initializer=keras.initializers.RandomNormal())
     ]
 )
-model.compile(loss=keras.losses.binary_crossentropy, optimizer=keras.optimizers.Adam(lr=1e-3), metrics=['accuracy'])
-model.fit(X_train, y_train, epochs=100, batch_size=20)
+model.compile(loss=keras.losses.binary_crossentropy, optimizer=keras.optimizers.Adam(lr=1e-2), metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=20, batch_size=20)
 y_pred = model.predict_classes(X_test)
 print('ACCURACY: {}'.format( 100 * np.sum(y_pred.ravel() == y_test) / len(y_test)))
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from matplotlib.colors import ListedColormap
 
 def plot_decisions_boundary(decision_function, X, Y, alpha=.8):
@@ -11,3 +12,27 @@ def plot_decisions_boundary(decision_function, X, Y, alpha=.8):
     Z = Z.reshape(xx.shape)
     plt.contourf(xx, yy, Z, alpha=alpha)
     plt.scatter(X[:,0], X[:,1], c=Y)
+
+def read_titanic_dataset(path, has_labels=True):
+    train = pd.read_csv(path)
+
+    #Deal with missing values!
+    train['Age'].fillna(train['Age'].mean(), inplace=True)
+    train['Fare'].fillna(train['Fare'].mean(), inplace=True)
+
+    #Encode categorical string into numbers
+    train = train.replace(to_replace={'Sex': {'male': 0, 'female':1}})
+    column_to_remove = ['Cabin', 'Name', 'Embarked', 'PassengerId', 'Ticket']
+    if has_labels:
+        column_to_remove.append('Survived')
+        labels = train['Survived']
+    train = pd.get_dummies(train, columns=['Pclass'])
+    #Remove not-so useful columns (except the ticket... we could infer something from it?) and Y (if present)
+    train.drop(column_to_remove,axis=1, inplace=True)
+    X = train.as_matrix() 
+    result = X if not has_labels else (X, labels.as_matrix().ravel()) 
+    return result
+
+def read_titanic_test_labels():
+    labels = pd.read_csv('Challenge/Titanic/gender_submission.csv')
+    return labels.as_matrix().ravel()
